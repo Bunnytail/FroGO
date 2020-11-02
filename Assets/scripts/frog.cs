e2ea1f5e6409d5cd8228d5f22c4f93d204d14812
Bunnytail <70060957+Bunnytail@users.noreply.github.com>
2020-11-02 19:40:13 +0100

    kamera, skok

--- Assets/scripts/frog.cs
+++ Assets/scripts/frog.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class frog : MonoBehaviour
 {
     private Rigidbody rigidbody;
+    private bool grounded = false;
+    
+
+
     public float speed;
+    public float jump;
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
@@ -14,15 +19,15 @@ public class frog : MonoBehaviour
     
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
     private void FixedUpdate()
     {
         //rigidbody.velocity = Vector3.forward * speed;
-        rigidbody.AddRelativeForce(Vector3.forward * speed, ForceMode.VelocityChange);
-        if (Input.GetKeyDown(KeyCode.Space))
+        //rigidbody.AddRelativeForce(Vector3.forward * speed, ForceMode.VelocityChange);
+        if (Input.GetKeyDown(KeyCode.Space)&&grounded)
         {
-            rigidbody.AddForce(Vector3.up * 400f);
+            rigidbody.AddForce(Vector3.up * jump, ForceMode.Impulse);
 
             Debug.Log("skacz");
         }
@@ -34,6 +39,24 @@ public class frog : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Rotate(new Vector3(0, 1, 0));
+        }
+
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "ground")
+        {
+            grounded = true;
+            
+        }
+    }
+    void OnCollisionExit(Collision other)
+    {
+        if (other.collider.tag == "ground")
+        {
+            grounded = false;
+           
         }
     }
 }
