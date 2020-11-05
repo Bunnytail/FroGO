3419c03e583352769b944043192dda1cb97ee2bc
WeronikaDo <weronikadobrenko@gmail.com>
2020-11-05 19:14:04 +0100

    kamera

---
+++ Assets/scripts/camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    private Transform lookAT;
+    private Vector3 offset;
+
+        // Start is called before the first frame update
+    void Start()
+    {
+        lookAT = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - lookAT.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = lookAT.position + offset;
+    }
+}
