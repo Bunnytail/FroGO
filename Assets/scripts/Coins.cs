c7cab591e5a1ddcfe7b71b12f20f82b61ed3664c
SaraGrzelak <xpikaczx@gmail.com>
2020-11-06 17:03:59 +0100

    test licznika

---
+++ Assets/scripts/Coins.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coins : MonoBehaviour
+{
+    public Text scoreTextPrefab;
+    public GameObject coin;
+	private int points = 0;
+	void Update()
+	{
+		scoreTextPrefab.text = "Score: " + points;
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            coin.SetActive(false);
+            points++;
+            Debug.Log("kolizja");
+
+        }
+    }
+}
