6bab516930fa5d50f36abe7fc3ee4e7b0ad969d0
WeronikaDo <weronikadobrenko@gmail.com>
2020-11-08 16:21:36 +0100

    Śmierć żabki - brak ruchu

---
+++ Assets/scripts/withdeadFrogController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class withdeadFrogController : MonoBehaviour
+{
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    public float moveSpeed = 1f;
+    private Animator animator;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            moveDirection.y = jumpSpeed;
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        moveDirection.x = 0;
+        moveDirection.z = 0;
+
+        animator.SetBool("jumping", controller.isGrounded == false);
+
+
+
+
+        moveDirection += transform.forward * moveSpeed;
+        animator.SetBool("walking", true);
+
+
+    
+
+
+        if (Input.GetKey(KeyCode.LeftArrow) && controller.isGrounded)
+        {
+            if (transform.position.y > -3.5)
+            {
+                transform.Rotate(new Vector3(0, -1, 0));
+            }
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) && controller.isGrounded)
+        {
+            if (transform.position.y > -3.5)
+            {
+                transform.Rotate(new Vector3(0, 1, 0));
+            }
+        }
+
+        controller.Move(moveDirection * Time.deltaTime);
+
+        if (transform.position.y < -3.5)
+        {
+            animator.SetBool("walking", false);
+            moveSpeed = 0;
+            jumpSpeed = 0;
+            
+        }
+        if (transform.position.y < -110.5)
+        {
+            moveDirection.y = 0;
+            animator.SetBool("jumping", controller.isGrounded == true);
+        }
+    }
+}
+
+
+
+
