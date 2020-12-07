b748ade5804a68f8c6e1422d3edf8e5c68dd0c70
SaraGrzelak <xpikaczx@gmail.com>
2020-12-07 09:05:26 +0100

    zaba chodzi i prawie skacze

---
+++ Assets/scripts/FrogControllerNew.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogControllerNew : MonoBehaviour
+{
+    public float gravity = 100.0f;
+
+    private CharacterController cc;
+    private Animator anim;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && cc.isGrounded)
+        {
+            anim.SetTrigger("Jump");
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0.0f, -1.0f, 0.0f));
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f));
+        }
+
+        if(cc.isGrounded)
+        {
+            moveDirection.y = 0.0f;
+        }
+        else 
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        cc.Move(moveDirection * Time.deltaTime);
+
+    }
+}
